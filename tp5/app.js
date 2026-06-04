ae52634184daa3d5010e7f7bbf5b66c765360606
alumno <52060292@ivt.edu.ar>
2026-06-04 09:07:31 -0300

    4 junio

--- tp5/app.js
+++ tp5/app.js
@@ -4,6 +4,10 @@ let suma=numero1 + numero2;
 let resta=numero1-numero2
 let multiplicacion= numero1*numero2
 let division= numero1/numero2
-
-console.log(suma);
-alert(suma)
+console.log("el resultado de la resta es:" +resta)
+console.log("el resultado de la suma es:"+suma);
+console.log("el resultado de la multiplicacion:"+multiplicacion)
+console.log("el resultado de la divisiones:"+division)
+let nombrealumno= eduardo
+let apellidoalumno=giraudo 
+let edadalumno=14
