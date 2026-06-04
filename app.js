aad0dc30495abd7eda71241f7f545994a718903a
alumno <52060292@ivt.edu.ar>
2026-06-04 07:58:45 -0300

    variable de suma,resta,multiplicacion y division

--- app.js
+++
@@ -1,9 +0,0 @@
-let numero1= 25;
-let numero2= 23;
-let resultado 
-
-resultado=numero1 + numero2;
-
-
-console.log(resultado);
-alert(resultado)
---
+++ tp5/app.js
@@ -0,0 +1,9 @@
+let numero1=20
+let numero2=10
+let suma=numero1 + numero2;
+let resta=numero1-numero2
+let multiplicacion= numero1*numero2
+let division= numero1/numero2
+
+console.log(suma);
+alert(suma)
