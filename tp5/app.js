db3aa2c55380e5eb120fb1c7124453ba6c1918b6
alumno <alumno@Instituto.local>
2026-06-11 08:18:27 -0300

    Update app.js

--- tp5/app.js
+++ tp5/app.js
@@ -8,6 +8,17 @@ console.log("el resultado de la resta es:" +resta)
 console.log("el resultado de la suma es:"+suma);
 console.log("el resultado de la multiplicacion:"+multiplicacion)
 console.log("el resultado de la divisiones:"+division)
-let nombrealumno= eduardo
-let apellidoalumno=giraudo 
-let edadalumno=14
+  
+
+let nombreAlumno="eduardo" 
+let apellidoAlumno="giraudo"
+let edadAlumno=14
+
+console.log("el Alumno: " +nombreAlumno + ' ' + apellidoAlumno+ " TIENE "+ edadAlumno + " años" );
+
+
+let precio1= 10
+let precio2= 15
+let precio3= 20
+ let total = precio1+ precio2 + precio3
+console.log(  "el total a pagar es"+ total)
