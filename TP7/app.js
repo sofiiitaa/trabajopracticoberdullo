223fdffb5a947f272ef41b29c2f7daa2a135b93b
alumno <alumno@Instituto.local>
2026-08-06 08:52:06 -0300

    6/08/26

---
+++ TP7/app.js
@@ -0,0 +1,26 @@
+let ejercicio1 = document.querySelector("#eje1")
+let botoneje1 = document.querySelector("#botonEje1")
+let edad = 26
+
+
+botoneje1.onclick = function () {
+    if (edad >= 18) {
+        ejercicio1.textContent = 'Sos mayor de edad'
+    } else {
+        ejercicio1.textContent = 'No sos mayor de edad'
+    }
+}
+let nombredeusuario = "Nahuel"
+let ejercicio2 = document.querySelector("#eje2")
+let botoneje2 = document.querySelector("#eje")
+
+
+botoneje2.onclick = function () {
+    if (nombredeusuario == "Nahuel") {
+        ejercicio2.textContent = "Bienvenido Nahuel,como estas?"
+
+    }
+    else {
+
+    }
+}
