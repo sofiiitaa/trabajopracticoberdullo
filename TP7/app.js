32bd7d6759ce042ee9242b12cc892379708b98b6
alumno <alumno@Instituto.local>
2026-08-13 09:20:22 -0300

    13- 08-26

--- TP7/app.js
+++ TP7/app.js
@@ -20,25 +20,64 @@ botoneje2.onclick = function () {
         ejercicio2.textContent = "Bienvenido Nahuel,como estas?"
 
     }
-    else {ejercicio2.textContent ="Bienvenido usuario"          
+    else {
+        ejercicio2.textContent = "Bienvenido usuario"
     }
 }
- let nombredeusuario ="marcos" + "nahuel"
- let ejercicio3 = document.querySelector("#eje3")
- let botoneje3 = document.querySelector("#eje")
- 
- botoneje3.onclick = function(){
-if (nombredeusuario = " marcos" +"nahuel"){
-ejercicio3.textContent= "bienvenido marcos" +"bienvenido nahuel"
-}                    
-else{ ejercicio3.textContent ="bienvenido usuario"
+let nombredeusuario2 = "marcos"
+let ejercicio3 = document.querySelector("#eje3")
+let botoneje3 = document.querySelector("#eje")
 
+botoneje3.onclick = function () {
+    if (nombredeusuario = " marcos" + "nahuel") {
+        ejercicio3.textContent = "bienvenido marcos" + "bienvenido nahuel"
+    }
+    else {
+        ejercicio3.textContent = "bienvenido usuario"
 
+    }
+}
+let numero = 0
+let botoneje4 = document.querySelector("#botoneje4")
+let eje4 = document.querySelector("#eje4")
+botoneje4.onclick = function () {
+    if (numero < 0) {
+        eje4.textcontent = "El número es negativo"
+    } else if (numero > 0) {
+        eje4.textContent = "el numero es positivo"
+    } else {
+        eje4.textContent = "el numero es 0"
 
+    }
+}
+let ejercicio = document.querySelector("#eje5")
+let botoneje5 = document.querySelector("#botoneje5")
+let edad2 = 80
 
+botoneje5.onclick = function () {
+    if ((edad >= 6) && (edad <= 11)) {
+        eje5.textContent = "la edad corresponde es a un niño"
+    } else if ((edad >= 12) && (edad <= 18)) {
+        eje5.textContent = "la edad correspnde a un adolecente"
+    } else if ((edad >= 19) && (edad <= 26)) {
+        eje5.textContent = "la edad corresponde a un adulto"
+    } else if (edad >= 60) {
+        eje5.textContent = "la edad corresponde a un anciano"
+    }
 }
 
+let eje7 = document.querySelector("#eje7")
+let botoneje7 = document.querySelector("#botoneje7")
+let contraseña = '123456'
+
+botoneje7.onclick = function () {
+    if (contraseña = 'secreto') {
+
+        eje7.textContent = 'acesso concedido'
+    }
+    else {
+        eje7.textContent = 'acesso denegado'
 
 
-
- }
+    }
+}
